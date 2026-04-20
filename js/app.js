f53f1741a0743c6fff25f59c3677fdd9c4395810
saulandresv <saul.vega2@gmail.com>
2026-04-20 09:18:11 -0400

    point API_URL to railway deployment

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const state = {
     year:     new Date().getFullYear()
 };
 
-const API_URL = '';
+const API_URL = 'https://testb-production-30a4.up.railway.app';
 const ALL_SLOTS = [
     '09:00','09:30','10:00','10:30','11:00','11:30',
     '12:00','12:30','13:00','13:30','14:00','14:30',
